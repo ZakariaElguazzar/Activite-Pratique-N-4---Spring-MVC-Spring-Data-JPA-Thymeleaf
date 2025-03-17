52f46a18470d1b42d84b2b43c6d9bebaa24ba815
Zakaria <elguazzarzakaria10@gmail.com>
2025-03-17 16:18:48 +0000

    Adding some users for test

--- src/main/java/com/example/activity3/Activity3Application.java
+++ src/main/java/com/example/activity3/Activity3Application.java
@@ -1,7 +1,13 @@
 package com.example.activity3;
 
+import com.example.activity3.Entities.Patient;
+import com.example.activity3.Repositories.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class Activity3Application {
@@ -9,5 +15,12 @@ public class Activity3Application {
     public static void main(String[] args) {
         SpringApplication.run(Activity3Application.class, args);
     }
-
+    @Bean
+    CommandLineRunner start(PatientRepository patientRepository) {
+        return args -> {
+            patientRepository.save(new Patient(null, "Mohamed", new Date(), false, 34));
+            patientRepository.save(new Patient(null, "Hanane", new Date(), false, 4321));
+            patientRepository.save(new Patient(null, "Imane", new Date(), true, 34));
+        };
+    }
 }
