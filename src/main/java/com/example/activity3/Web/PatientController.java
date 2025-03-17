9b2c97399c0ebd4291e095503f4448c8e4fc2ff8
Zakaria <elguazzarzakaria10@gmail.com>
2025-03-17 23:04:23 +0000

    Some upgrades for index and adding delete funciton

--- src/main/java/com/example/activity3/Web/PatientController.java
+++ src/main/java/com/example/activity3/Web/PatientController.java
@@ -3,11 +3,12 @@ package com.example.activity3.Web;
 import com.example.activity3.Entities.Patient;
 import com.example.activity3.Repositories.PatientRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @AllArgsConstructor
@@ -15,9 +16,20 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/patients")
-    public String index(Model model) {
-        List<Patient> patientsList = patientRepository.findAll();
-        model.addAttribute("listPatients", patientsList);
+    public String index(Model model,
+                        @RequestParam(name = "page",defaultValue = "0") int page,
+                        @RequestParam(name = "size",defaultValue = "4") int size,
+                        @RequestParam(name = "keyword",defaultValue = "") String kw) {
+        Page<Patient> pagePatients = patientRepository.findByNomContains(kw,PageRequest.of(page, size));
+        model.addAttribute("listPatients", pagePatients.getContent());
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("keyword", kw);
         return "Patients";
+    }
+    @GetMapping("/delete")
+    public String delete(Long id) {
+        patientRepository.deleteById(id);
+        return "redirect:/patients";
     }
 }
