a08ffff34a12be95526b0d660e631b7e8ea524a5
Zakaria <elguazzarzakaria10@gmail.com>
2025-03-17 23:03:35 +0000

    Adding the findByNomContains method

--- src/main/java/com/example/activity3/Repositories/PatientRepository.java
+++ src/main/java/com/example/activity3/Repositories/PatientRepository.java
@@ -1,9 +1,13 @@
 package com.example.activity3.Repositories;
 
 import com.example.activity3.Entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Page<Patient> findByNomContains(String keyword,Pageable pageable);
 }
