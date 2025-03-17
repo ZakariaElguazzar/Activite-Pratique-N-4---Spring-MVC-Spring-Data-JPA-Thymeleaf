0bd5ca4f142c1220ae71fd6a2893ceb7b5aee542
Zakaria <elguazzarzakaria10@gmail.com>
2025-03-17 16:16:56 +0000

    Creation of Patient Repository entity

---
+++ src/main/java/com/example/activity3/Repositories/PatientRepository.java
@@ -0,0 +1,9 @@
+package com.example.activity3.Repositories;
+
+import com.example.activity3.Entities.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+}
