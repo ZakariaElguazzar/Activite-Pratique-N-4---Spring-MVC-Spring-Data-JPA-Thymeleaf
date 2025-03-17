39551fbbc3e3e951c1ccc4b23ac9166f1dc84a73
Zakaria <elguazzarzakaria10@gmail.com>
2025-03-17 16:45:36 +0000

    loading the patinets from the database and put in them in the model

--- src/main/java/com/example/activity3/Web/PatientController.java
+++ src/main/java/com/example/activity3/Web/PatientController.java
@@ -1,10 +1,13 @@
 package com.example.activity3.Web;
 
+import com.example.activity3.Entities.Patient;
 import com.example.activity3.Repositories.PatientRepository;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -12,7 +15,9 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/patients")
-    public String index() {
-        return "patients";
+    public String index(Model model) {
+        List<Patient> patientsList = patientRepository.findAll();
+        model.addAttribute("listPatients", patientsList);
+        return "Patients";
     }
 }
