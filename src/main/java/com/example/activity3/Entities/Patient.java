58cc1eb3708314c9d522b4dccf9f38421621ec54
Zakaria <elguazzarzakaria10@gmail.com>
2025-03-21 14:59:21 +0000

    adding the @NotEmpty @Size @DecimalMin for some attributs

--- src/main/java/com/example/activity3/Entities/Patient.java
+++ src/main/java/com/example/activity3/Entities/Patient.java
@@ -1,6 +1,9 @@
 package com.example.activity3.Entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,10 +17,13 @@ import java.util.Date;
 public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty
+    @Size(min = 0, max = 40,message = "{Size.patient.name}")
     private String nom;
     @Temporal(TemporalType.DATE)
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateNaissance;
     private boolean malade;
+    @DecimalMin("0")
     private int score;
 }
