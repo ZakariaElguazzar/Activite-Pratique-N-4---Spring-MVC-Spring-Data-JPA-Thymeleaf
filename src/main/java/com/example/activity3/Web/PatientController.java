f46d8dbf58d217639b61d21f0d8af0371b85fdce
Zakaria <elguazzarzakaria10@gmail.com>
2025-03-28 22:57:56 +0000

    Giving authorization with annotation @PreAuthorize

--- src/main/java/com/example/activity3/Web/PatientController.java
+++ src/main/java/com/example/activity3/Web/PatientController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,6 +19,7 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/user/patients")
+    @PreAuthorize("hasRole('ROLE_USER')")
     public String index(Model model,
                         @RequestParam(name = "page",defaultValue = "0") int page,
                         @RequestParam(name = "size",defaultValue = "4") int size,
@@ -30,6 +32,7 @@ public class PatientController {
         return "Patients";
     }
     @GetMapping("/admin/delete")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String delete(@RequestParam(name = "id") Long id,
                          @RequestParam(name = "page",defaultValue = "0") int page,
                          @RequestParam(name = "keyword",defaultValue = "") String kw) {
@@ -46,6 +49,7 @@ public class PatientController {
         return "formPatients";
     }
     @GetMapping("/admin/editPatient")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String editPatient(@RequestParam(name = "id") Long id,Model model,@RequestParam(name = "keyword")String keyword,@RequestParam(name = "page")int page) {
         Patient patient = patientRepository.findById(id).orElse(null);
         if (patient == null) throw new RuntimeException("Patient not found");
@@ -55,6 +59,7 @@ public class PatientController {
         return "editPatient";
     }
     @PostMapping("/admin/save")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String save(@ModelAttribute @Valid Patient Patient, BindingResult bindingResult, Model model,@RequestParam(defaultValue = "0") int page,@RequestParam(defaultValue = "") String keyword) {
         if(bindingResult.hasErrors()) return "formPatients";
         patientRepository.save(Patient);
